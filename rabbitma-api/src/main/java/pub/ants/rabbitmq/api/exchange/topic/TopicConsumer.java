0580d32cadc4f91e21ccb0e8db53b328de26e6fa
iearl <dc_magaowei@163.com>
2019-07-12 15:03:18 +0800

    RabbitMQ topic模式代码

---
+++ rabbitma-api/src/main/java/pub/ants/rabbitmq/api/exchange/topic/TopicConsumer.java
@@ -0,0 +1,16 @@
+package pub.ants.rabbitmq.api.exchange.topic;
+
+/**
+ * @ProjectName: rabbitma-api
+ * @Package: pub.ants.rabbitmq.api.exchange.topic
+ * @ClassName: TopicConsumer
+ * @Description: java类作用描述
+ * @Author: 唐朝
+ * @CreateDate: 2019/7/12 14:46
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/7/12 14:46
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class TopicConsumer {
+}
