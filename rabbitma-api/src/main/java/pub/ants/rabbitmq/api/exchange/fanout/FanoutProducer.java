33650a0ed4b351e0f47ff79439fccb2a4df6b5dc
iearl <dc_magaowei@163.com>
2019-07-12 15:44:53 +0800

    fanout模式交换机

---
+++ rabbitma-api/src/main/java/pub/ants/rabbitmq/api/exchange/fanout/FanoutProducer.java
@@ -0,0 +1,16 @@
+package pub.ants.rabbitmq.api.exchange.fanout;
+
+/**
+ * @ProjectName: rabbitma-api
+ * @Package: pub.ants.rabbitmq.api.exchange.fanout
+ * @ClassName: FanoutProducer
+ * @Description: java类作用描述
+ * @Author: 唐朝
+ * @CreateDate: 2019/7/12 15:04
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/7/12 15:04
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class FanoutProducer {
+}
